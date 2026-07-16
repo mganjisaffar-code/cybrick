45d5cfb9cb835e1e8671efe484e4d51ba5644850
mganjisaffar-code <m.ganjisaffar@gmail.com>
2026-07-16 13:56:19 +0330

    Add alert management module

---
+++ src/alert_manager.py
@@ -0,0 +1,19 @@
+"""
+Cybrick Alert Manager
+Version 0.1
+"""
+
+
+class AlertManager:
+
+    def __init__(self):
+        self.alerts = []
+
+    def add_alert(self, alert):
+        self.alerts.append(alert)
+
+    def count(self):
+        return len(self.alerts)
+
+    def get_all(self):
+        return self.alerts
