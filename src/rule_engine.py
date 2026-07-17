4d9ee26dd7e602b65a44ca798e5ebc8b0bf06dff
mganjisaffar-code <m.ganjisaffar@gmail.com>
2026-07-17 15:57:47 +0330

    Implement first Modbus detection rule

--- src/rule_engine.py
+++ src/rule_engine.py
@@ -1,6 +1,6 @@
 """
 Cybrick Rule Engine
-Version 0.1
+Version 0.2
 """
 
 
@@ -8,4 +8,11 @@ class RuleEngine:
     """Initial rule engine for ICS/SCADA."""
 
     def evaluate(self, features):
+
+        if features.get("function_code") == 8:
+            return {
+                "severity": "high",
+                "mitre": "T0804"
+            }
+
         return None
